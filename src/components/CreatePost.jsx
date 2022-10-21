d2b720bd44a50dd88f9cb2112997e8fb11a1c4c4
ADMIN <admin@ADMINs-Mac-mini.local>
2022-10-21 09:32:41 -0500

    create post working

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 export default function CreatePost() {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [location, setLocation] = useState('');
+    const [willDeliver, setWillDeliver] = useState(true);
+    async function handleSubmit(event) {
+        event.preventDefault();}
   return (
     <div>CreatePost
-        <form action="">
-        <input placeholder='title' /> 
-          <input placeholder='description' /> 
-          <input placeholder='price' /> 
-          <input placeholder='location' /> 
+        <form onSubmit={handleSubmit}>
+        <input placeholder='title' value={title}
+        onChange={(e) => setTitle(e.target.value)} label='Title'
+        /> 
+          <input placeholder='description' value={description}
+        onChange={(e) => setDescription(e.target.value)} label='Description'
+        />  
+          <input placeholder='price' value={price}
+        onChange={(e) => setPrice(e.target.value)} label='Price'
+        />  
+          <input placeholder='location' value={location}
+        onChange={(e) => setLocation(e.target.value)} label='Location'
+        />  
+        <button type='submit'>Submit</button>
         </form>
     </div>
   )
